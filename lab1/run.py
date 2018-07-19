d71c882b339eda4a9f724265bef5914e52c153dc
KokeCacao <1412042720@qq.com>
2018-07-18 20:52:26 -0700

    finish all project

--- lab1/run.py
+++ lab1/run.py
@@ -72,15 +72,15 @@ class RobotBehaviorThread(threading.Thread):
           #   wheel_left = proximity_left *10
           #   wheel_right = proximity_right *10
           # # Line Follow
-          if left_detection == True and right_detection == False:
-            wheel_left = 50 #turning left
-            wheel_right = 10
-          elif left_detection == False and right_detection == True:
-            wheel_left = 10
-            wheel_right = 50 #turning right
-          else: # IDK what to do
-            wheel_left = 50
+          if left_detection == False and right_detection == True:
+            wheel_left = 10 #turning left
             wheel_right = 50
+          elif left_detection == True and right_detection == False:
+            wheel_left = 50
+            wheel_right = 10 #turning right
+          else: # IDK what to do
+            wheel_left = 10
+            wheel_right = 10
 
           robot.set_wheel(0, wheel_left)
           robot.set_wheel(1, wheel_right)
